6af797858f1f4b6bb3c850665279bcea740303ff
os65950034-star <os65950034@gmail.com>
2026-07-21 14:14:57 +0500

    Update plainapp.py

--- plainapp.py
+++ plainapp.py
@@ -609,7 +609,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    if __name__ == "__main__":
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
